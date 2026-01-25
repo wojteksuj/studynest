a440f1040299a63f530ac662c669dad20af4d258
wojteksuj <wojciechsuj@gmail.com>
2026-01-25 14:04:55 +0100

    Initial commit with updated project structure

---
+++ src/main/java/org/example/studynest/StudyNestApplication.java
@@ -0,0 +1,13 @@
+package org.example.studynest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyNestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyNestApplication.class, args);
+    }
+
+}
