535051e16801b95180151a868e8ed5ccfc44c2f1
wojteksuj <wojciechsuj@gmail.com>
2026-01-27 21:31:35 +0100

    Add dto for create user

---
+++ src/main/java/org/example/studynest/dto/request/CreateUserRequestDTO.java
@@ -0,0 +1,40 @@
+package org.example.studynest.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class CreateUserRequestDTO {
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
