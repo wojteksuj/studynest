f56c47835afa9127bafca8d5f4fd1a3b1da006c7
wojteksuj <wojciechsuj@gmail.com>
2026-01-27 12:47:12 +0100

    Update UserRepository

--- src/main/java/org/example/studynest/repository/UserRepository.java
+++ src/main/java/org/example/studynest/repository/UserRepository.java
@@ -3,9 +3,12 @@ package org.example.studynest.repository;
 import org.example.studynest.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
