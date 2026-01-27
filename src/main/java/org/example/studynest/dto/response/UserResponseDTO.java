14ed76acaf13e556ceaaaa8922e1bf62341cd520
wojteksuj <wojciechsuj@gmail.com>
2026-01-27 21:31:30 +0100

    Add dto for get user

---
+++ src/main/java/org/example/studynest/dto/response/UserResponseDTO.java
@@ -0,0 +1,43 @@
+package org.example.studynest.dto.response;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public class UserResponseDTO {
+    private UUID id;
+    private String username;
+    private String email;
+    private Instant registrationDate;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Instant getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Instant registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+}
