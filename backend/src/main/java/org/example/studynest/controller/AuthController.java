0f40d563a0d22c8e27f56a4b2da2f9f32188990b
wojteksuj <wojciechsuj@gmail.com>
2026-02-17 15:13:03 +0100

    Add endpoint for user registration

--- backend/src/main/java/org/example/studynest/controller/AuthController.java
+++ backend/src/main/java/org/example/studynest/controller/AuthController.java
@@ -1,13 +1,15 @@
 package org.example.studynest.controller;
 
+import jakarta.validation.Valid;
 import org.example.studynest.dto.request.LoginUserDTO;
+import org.example.studynest.dto.request.RegisterUserDTO;
+import org.example.studynest.dto.response.UserResponseDTO;
 import org.example.studynest.security.JwtService;
+import org.example.studynest.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -17,11 +19,21 @@ public class AuthController {
 
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final UserService userService;
 
     public AuthController(AuthenticationManager authenticationManager,
-                          JwtService jwtService) {
+                          JwtService jwtService, UserService userService) {
         this.authenticationManager = authenticationManager;
         this.jwtService = jwtService;
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserResponseDTO createUser(
+            @Valid @RequestBody RegisterUserDTO dto
+    ) {
+        return userService.register(dto);
     }
 
     @PostMapping("/login")
