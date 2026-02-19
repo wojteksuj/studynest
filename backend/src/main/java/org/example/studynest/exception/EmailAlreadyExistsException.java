54fbf07794b251580fa84ae35e7ca46bbf3b3645
wojteksuj <wojciechsuj@gmail.com>
2026-02-19 14:00:22 +0100

    Delete custom EmailAlreadyExistsException exception

--- backend/src/main/java/org/example/studynest/exception/EmailAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package org.example.studynest.exception;
-
-public class EmailAlreadyExistsException extends RuntimeException {
-    public EmailAlreadyExistsException(String email){
-        super("User with provided email: " + email + " already exists");
-    }
-}
