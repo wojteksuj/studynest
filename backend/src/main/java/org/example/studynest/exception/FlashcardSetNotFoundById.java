15c85f2076ff4944cccc1ddb6d189ceb0b7a1327
wsujczynski <wojciechsuj@gmail.com>
2026-04-29 16:51:24 +0200

    Remove unused imports

--- backend/src/main/java/org/example/studynest/exception/FlashcardSetNotFoundById.java
+++ backend/src/main/java/org/example/studynest/exception/FlashcardSetNotFoundById.java
@@ -1,7 +1,5 @@
 package org.example.studynest.exception;
 
-import java.util.UUID;
-
 public class FlashcardSetNotFoundById extends RuntimeException {
     public FlashcardSetNotFoundById() {
         super("Flashcard set with provided id does not exist.");
